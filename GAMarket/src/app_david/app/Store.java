8db4c6718da2e1d7f428f171793d50e6ab742ee0
Jerome Joshua Fulgado <jfulgado4@horizon.csueastbay.edu>
2020-04-21 18:19:31 -0700

    Created StoreGUI with added constructor

--- GAMarket/src/app_david/app/Store.java
+++ GAMarket/src/app_david/app/Store.java
@@ -10,13 +10,18 @@ public class Store {
     // member function which will store a feed of games
     // for users to look at, get recommended features
     private Game[] feed;
+    private int numGames;
 
+    // Default constructor
+    public Store() {
+        numGames = 0;
+        feed = new Game[7];
+    }
 
-    public GameCollection purchaseGame(String genre){
+    public void purchaseGame(String title){
         // Store function will let a user purchase a game
         // from a gamecollection object
         GameCollection test = new GameCollection();
-        return test;
     }
 
     public Game searchGames(String name) {
@@ -30,6 +35,10 @@ public class Store {
         // Enter a games profile, showing related games
         // that the developer has also created and
         // recommended game
+    }
+
+    public void loadGameData(String filename){
+
     }
 
 }
---
+++ GAMarket/src/app_david/app/StoreGUI.java
@@ -0,0 +1,10 @@
+package app;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+
+public class StoreGUI {
+
+}
