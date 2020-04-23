df21b19f0d512b41a4190bf2ab1f9f48ca32107f
Christian Alcalde <cc.alcalde@yahoo.com>
2020-04-23 15:45:25 -0700

    Replaced UserGUI class with ProfileGUI class

---
+++ GAMarket/src/app/sample/GAMarket/src/app/ProfileGUI.java
@@ -0,0 +1,14 @@
+package sample.GAMarket.src.app;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class ProfileGUI extends JFrame {
+    public static void main(String[] args) throws IOException {
+            JFrame frame = new JFrame("Profile");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400,300);
+
+
+    }
+}
