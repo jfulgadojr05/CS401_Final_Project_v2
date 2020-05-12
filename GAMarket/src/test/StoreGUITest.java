8b6fd01606194cb40c049005ff066ab9ebd55348
Jerome Joshua Fulgado <jfulgado4@horizon.csueastbay.edu>
2020-05-12 01:26:03 -0700

    Created getAllGameTest case

--- GAMarket/src/test/StoreGUITest.java
+++ GAMarket/src/test/StoreGUITest.java
@@ -1,12 +1,68 @@
 package test;
 
+import app.Game;
+import db.DBHelper;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class StoreGUITest {
 
+    DBHelper testDB = new DBHelper();
+    ResultSet resultTest;
+    PreparedStatement prepareStmtTest;
+    Statement stmtTest;
+    Connection connectionTest;
+
+
+
+    @Before
+    public void initializeDB() throws SQLException {
+        testDB.createNewDatabase();
+        testDB.createAllTables();
+    }
+//
+//    @After
+//    public void closeDB() throws  SQLException {
+//        resultTest.close();
+//        prepareStmtTest.close();
+//        stmtTest.close();
+//        connectionTest.close();
+//    }
+
+
+
     @Test
-    public void getAllGameTest(){
-        // return
+    public void getAllGameTest() throws SQLException {
+        // Check if a game exists in all the games
+        // will also check if a game does not exist
+        boolean isPresent = false;
+        int testGameID = 1002;
+        int testNonGameID = 1010;
+        Game[] allGameTest = testDB.getAllGameObjects();
+        for (Game game : allGameTest) {
+            if (game.getId() == testGameID) {
+                isPresent = true;
+                break;
+            }
+        }
+
+        assertTrue("Game is not present",isPresent);
+
+        isPresent = false;
+        for (Game game : allGameTest) {
+            if (game.getId() == testNonGameID) {
+                isPresent = true;
+                break;
+            }
+        }
+
+        assertFalse("Game is present",isPresent);
+
     }
 
     @Test
@@ -34,7 +90,8 @@ public class StoreGUITest {
         // return
     }
 
-    @Test void purchaseGameTest(){
+    @Test
+    public void purchaseGameTest(){
 
     }
 }
