5f0993bc346e153cc68f3fd7c6c38de66bd602c1
Jerome Joshua Fulgado <jfulgado4@horizon.csueastbay.edu>
2020-05-12 08:57:40 -0700

    Added comments to game collection file

--- GAMarket/src/app/GameCollection.java
+++ GAMarket/src/app/GameCollection.java
@@ -36,43 +36,75 @@ public class GameCollection {
         this.numberOfGames = numberOfGames;
     }
 
+
     public void deleteGame(String gameName, DBHelper dbh, String userID) throws SQLException {
+        // Delete a users game from their collection
+        // from the database using gamename, dbhelper, and user id
+
+        // Confirm message
         String confirmMessage = "Delete " + gameName + " from library?";
+
+        // joptionpane to delete game from user collection
         int confirmOption = JOptionPane.showConfirmDialog(null, confirmMessage);
         if (confirmOption == JOptionPane.YES_OPTION){
+
+            // dbh delete game function
             dbh.deleteGame(gameName, userID);
         }
     }
 
     public String searchForGame(String gameTitle, DBHelper dbh) throws SQLException {
+        // Return a string if the game was found or not
+
+        // Creating local variables
         String temp = null;
+
+        // Getting game object array from database
         Game[] tempGameArray = dbh.getAllGameObjects();
+
+        // for loop to see if the game is present in game table
         for (Game game : tempGameArray) {
+
+            // if yes
             if (game.getName().toUpperCase().equals(gameTitle.toUpperCase())) {
+
+                // Set temp string to game id
                 temp = Integer.toString(game.getId());
                 break;
             }
         }
+
+        // if no game was found
         if(temp == null){
             temp = "Not found";
         }
+
+        // return temp
         return temp;
     }
     public void purchaseGame(String gameName, String userID, DBHelper dbh) throws SQLException {
-        // Function to purchase game, which adds to store
+        // Function to purchase game, which adds to users game collection after purchase
+
+        // Local variables
         String confirmMessage = "Purchase " + gameName + "?";
+
+        // joption pane to confirm game purchase
         int confirmOption = JOptionPane.showConfirmDialog(null, confirmMessage);
         if (confirmOption == JOptionPane.YES_OPTION){
+
+            // add game to users game collection
             dbh.purchaseGame(gameName, userID);
         }
 
     }
     public JList<String> filterGameGenre(String aGenre, DBHelper dbh) throws SQLException {
+        // Return a filtered JList by genre
         return dbh.getFilterGameGenre(aGenre);
 
     }
 
     public JList<String> filterGameRating(String aRating, DBHelper dbh) throws SQLException {
+        // Return a filtered JList by rating
         return dbh.getFilterGameRating(aRating);
     }
 
